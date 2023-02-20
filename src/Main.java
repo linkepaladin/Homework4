6840bdb4b2c467e703b6e402e7123712ac9c768e
Linke <linke@duck.com>
2023-02-21 02:36:44 +0300

    task1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+        public static void task1() {
+            byte age1 = 26;
+            byte age2 = 7;
+            if (age1 >= 18) {
+                System.out.println("Если возраст человека равен " + age1 + " лет, то он достиг совершеннолетия.");
+            }
+            if (age2 < 18) {
+                System.out.println("Если возраст человека равен " + age2 + " лет, то он недостиг совершеннолетия, " +
+                        "нужно немного подождать.");
+            }
+        }
 }
