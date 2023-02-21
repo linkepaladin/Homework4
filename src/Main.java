73f47a6db917579bd36e8a0ffc05881ff13a90c4
Linke <linke@duck.com>
2023-02-21 23:38:55 +0300

    task2 completed

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,17 @@ public class Main {
             byte age = 17;
             if (age >= 18) {
                 System.out.println("Если возраст человека равен " + age + " лет, то он достиг совершеннолетия.");
-            }
-            else {
-                System.out.println("Если возраст человека равен " + age + " лет, то он недостиг совершеннолетия, " +
+                } else {
+                    System.out.println("Если возраст человека равен " + age + " лет, то он недостиг совершеннолетия, " +
                         "нужно немного подождать.");
-            }
+                }
         }
-        public static void task2(){
+        public static void task2() {
             byte temp = 2;
             if (temp >= 5) {
                 System.out.println("На улице " + temp + " градусов, можно идти без шапки");
-            }
-            else {
-                System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
-            }
-            }
+                } else {
+                    System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
+                }
         }
+}
