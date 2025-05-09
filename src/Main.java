efff293380e88469b39b9c80aadb5892fd672a9f
linke <linke@duck.com>
2025-05-09 13:51:55 +0300

    Complete Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello, World!!!");
+            task1();
         }
+    public static void task1() {
+        int operationSystem = 4;
+        switch (operationSystem) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Для данной операционной системы нет приложения");
+
+        }
+    }
 }
