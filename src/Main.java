fe3d4a8046c35614f6a49003628ebc0d4e06e19c
linke <linke@duck.com>
2025-05-09 14:28:00 +0300

    Complete Task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
             task1();
             task2();
+            task3();
         }
     public static void task1() {
         int operationSystem = 4;
@@ -34,6 +35,21 @@ public class Main {
             }
         } else {
             System.out.println("Для данной операционной системы нет приложения");
+        }
+    }
+    public static void task3() {
+        int year = 1599;
+        int leapYear = 1584;
+        if (year >= leapYear) {
+            if (year % 4 == 0 && year % 100 != 0) {
+                System.out.println("Это високосный год");
+            } else if (year % 400 == 0) {
+                System.out.println("Это високосный год");
+            } else {
+                System.out.println("Это не високосный год");
+            }
+        } else {
+            System.out.println("Это не високосный год");
         }
     }
 }
