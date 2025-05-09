3fdd3ebd8ad9216347c38349422eddabaa1bbc4f
linke <linke@duck.com>
2025-05-09 14:10:50 +0300

    Complete Task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
             task1();
+            task2();
         }
     public static void task1() {
         int operationSystem = 4;
@@ -14,6 +15,25 @@ public class Main {
             default:
                 System.out.println("Для данной операционной системы нет приложения");
 
+        }
+    }
+    public static void task2() {
+        int clientDeviceYear = 2013;
+        int operationSystem = 1;
+        if (operationSystem == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientDeviceYear < 2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (operationSystem == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Для данной операционной системы нет приложения");
         }
     }
 }
